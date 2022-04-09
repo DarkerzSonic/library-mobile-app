e64e8c7c1ba5dd23502bb2bb2fb716a66bbc0b18
ASUS <cweijie64@gmail.com>
2022-04-09 17:57:39 +0800

    amitofu

--- app/src/main/java/my/edu/utar/libraryapp/fragment_books.java
+++ app/src/main/java/my/edu/utar/libraryapp/fragment_books.java
@@ -2,11 +2,26 @@ package my.edu.utar.libraryapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +30,14 @@ import android.view.ViewGroup;
  */
 public class fragment_books extends Fragment {
 
+    private EditText title, author, year, isbn, pages, status;
+    RecyclerView recyclerView;
+    BookAdapter adapter;
+    DAOBook dao;
+    String key = null;
+    SearchView searchView;
+
+    private Button submit;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -23,7 +46,7 @@ public class fragment_books extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    ArrayList<Book> books = new ArrayList<>();
     public fragment_books() {
         // Required empty public constructor
     }
@@ -53,12 +76,121 @@ public class fragment_books extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_books, container, false);
+        View view =  inflater.inflate(R.layout.fragment_books, container, false);
+
+//        title = (EditText) view.findViewById(R.id.title);
+//
+//        author = (EditText) view.findViewById(R.id.author);
+//
+//        year = (EditText) view.findViewById(R.id.year);
+//
+//        isbn = (EditText) view.findViewById(R.id.isbn);
+//
+//        pages = (EditText) view.findViewById(R.id.pages);
+//
+//        status = (EditText) view.findViewById(R.id.status);
+//
+//        submit = (Button) view.findViewById(R.id.add_submit);
+//
+//
+//        DAOBook dao = new DAOBook();
+//
+//
+//
+//        submit.setOnClickListener(view1 -> {
+//            Book book = new Book(title.getText().toString(), author.getText().toString(), Integer.parseInt(year.getText().toString()), isbn.getText().toString(), Integer.parseInt(pages.getText().toString()), Boolean.parseBoolean(status.getText().toString()));
+//
+//            dao.add(book).addOnSuccessListener(new OnSuccessListener<Void>() {
+//                @Override
+//                public void onSuccess(Void unused) {
+//                    Toast.makeText(getActivity(),"Record is inserted",Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//        });
+
+        searchView = (SearchView) view.findViewById(R.id.sv);
+
+        recyclerView = view.findViewById(R.id.rv);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(manager);
+        adapter = new BookAdapter(getActivity());
+        recyclerView.setAdapter(adapter);
+        dao = new DAOBook();
+        loadData();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                filter(s);
+                return false;
+            }
+        });
+
+
+        return view;
+
+    }
+
+    private void filter(String text) {
+        // creating a new array list to filter our data.
+        ArrayList<Book> filteredlist = new ArrayList<>();
+
+        // running a for loop to compare elements.
+        for (Book item : books) {
+            // checking if the entered string matched with any item of our recycler view.
+            if (item.getTitle().toLowerCase().contains(text.toLowerCase())) {
+                // if the item is matched we are
+                // adding it to our filtered list.
+                filteredlist.add(item);
+            }
+        }
+        if (filteredlist.isEmpty()) {
+            // if no item is added in filtered list we are
+            // displaying a toast message as no data found.
+            Toast.makeText(getContext(), "No Data Found..", Toast.LENGTH_SHORT).show();
+        } else {
+            // at last we are passing that filtered
+            // list to our adapter class.
+            adapter.filterList(filteredlist);
+        }
+    }
+
+    private void loadData() {
+        dao.get(key).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot data : snapshot.getChildren()){
+                    Book book = data.getValue(Book.class);
+                    book.setKey(data.getKey());
+                    books.add(book);
+                    key = data.getKey();
+
+                }
+                adapter.setItems(books);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
+        });
     }
 }
